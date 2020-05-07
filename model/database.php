b9b1888a3ffdf40103278340543000a1e91591c8
adbookerfhsu <58403630+adbookerfhsu@users.noreply.github.com>
2020-05-06 20:07:52 -0500

    updating with heroku credentials

--- model/database.php
+++ model/database.php
@@ -1,22 +1,22 @@
 <?php
     //local development server connection
-    $dsn = 'mysql:host=localhost;dbname=amysmoviequotes';
-    $username = 'root';
+    //$dsn = 'mysql:host=localhost;dbname=amysmoviequotes';
+    //$username = 'root';
     
 
     // Heroku connection
     
-    //$dsn = 'mysql:host=o677vxfi8ok6exrd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=YourJawsDBdbname';
-    //$username = 'xk1zadb0rkai9ro3';
-    //$password = 'chps6e6uir8o19l0';
+    $dsn = 'mysql:host=ijj1btjwrd3b7932.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=jqjwqjkmgmhjdh3c';
+    $username = 'wbw4wcacgmkn2no8';
+    $password = 'x3v6w99cpfdr108c';
     
     try {
         //local development server connection
         //if using a $password, add it as 3rd parameter
-       $db = new PDO($dsn, $username);
+       //$db = new PDO($dsn, $username);
 
         // Heroku connection
-        //$db = new PDO($dsn, $username, $password);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
